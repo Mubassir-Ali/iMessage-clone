f49e7957a15e4bb7a23c061d85c7fe2bfe793981
Mubassir-Ali <mubassir11@gmail.com>
2021-04-05 21:03:14 +0500

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import Imessage from './app/components/Imessage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+
+      <Imessage/>
     </div>
   );
 }
---
+++ src/app/components/SidebarChat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SidebarChat.css'
+
+function SidebarChat() {
+    return (
+        <div className='sidebar__chat'>
+            
+        </div>
+    )
+}
+
+export default SidebarChat
