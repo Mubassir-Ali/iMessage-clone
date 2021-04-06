92f3062290a0049ed467868bb82815136e82c4d2
Mubassir-Ali <mubassir11@gmail.com>
2021-04-06 20:01:38 +0500

    add firebase user Authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import Imessage from './components/Imessage'
+import React,{useEffect} from 'react';
+import Imessage from './components/Imessage';
 import './App.css';
-
+import { useSelector,useDispatch } from 'react-redux';
+import { selectUser,login,logout } from './features/userSlice';
+import Login from './components/Login';
+import { auth } from './firebase';
 
 function App() {
-  return (
-    <div className="App">
+	const user = useSelector(selectUser);
+  const dispatch =useDispatch()
 
-      <Imessage/>
-    </div>
-  );
+	useEffect(() => {
+		auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				//user is logged in
+        dispatch(login({
+          uid:authUser.uid,
+          photo:authUser.photoURL,
+          email:authUser.email,
+          displayName:authUser.displayName,
+
+        }))
+			} else {
+				//user is logged out
+
+        dispatch(logout)
+			}
+		});
+	}, []);
+	return <div className="App">{user ? <Imessage /> : <Login />}</div>;
 }
 
 export default App;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,14 +2,18 @@ import { Avatar, IconButton, Input } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import RateReviewOutlinedIcon from '@material-ui/icons/RateReviewOutlined';
 import React from 'react';
-
+import {useSelector} from 'react-redux'
 import './Sidebar.css';
 import SidebarChat from './SidebarChat';
+
+import {selectUser} from '../features/userSlice'
+
 function Sidebar() {
+	const user =useSelector(selectUser)
 	return (
 		<div className="sidebar">
 			<div className="sidebar__header">
-				<Avatar className='sidebar__avatar' />
+				<Avatar src={user.photo} className='sidebar__avatar' />
 				<div className="sidebar__input">
 					<SearchIcon />
 					<input placeholder="Search" />
