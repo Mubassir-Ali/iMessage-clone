197d2124706df2c641999ba7c24279d34a19ad01
Mubassir-Ali <mubassir11@gmail.com>
2021-04-07 14:59:48 +0500

    add chatInfo in sidebar chat

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,19 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
+import {useSelector} from 'react-redux'
+import {selectUser} from '../features/userSlice'
 import "./Message.css";
 
 function Message({ id, contents:{timestamp,photo,email,uid,message,displayName} }) {
+
+    const user=useSelector(selectUser)
+
+
   return (
-    <div className="message">
-    <Avatar src={photo}/>
+    <div className={`message ${user.email===email && 'message__sender'}`}>
+    <Avatar src={photo} className='message__photo'/>
       <p>{message} </p>
-      <small>{new Date(timestamp?.toDate()).toLocaleDateString()}</small>
+      <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
     </div>
   );
 }
